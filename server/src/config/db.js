37d8e96ed92ec5bd65a7183841557d02e85afd41
darrylanding1 <darrylbacolod12@gmail.com>
2026-03-31 05:51:18 +0800

    add db env debug logs

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,6 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('DB CONFIG DEBUG:', {
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+});
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 3306,
