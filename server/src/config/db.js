1fe57ba9a1bb322d3c890071b422d3e3f50f5cb6
darrylanding1 <darrylbacolod12@gmail.com>
2026-03-31 05:40:06 +0800

    updated db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,9 +5,14 @@ dotenv.config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
 });
 
 export default db;
